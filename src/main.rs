39717d9251f2c1ec1d10caefeb4b76bb1768b86d
UnlimitedFrames <21beaps@gmail.com>
2022-07-25 23:36:29 +1200

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    variables()
+}
+
+fn variables() {
+    let mut x = 5
+    println!("x is {x}");
+    {
+        let x = 54;
+        println!("x is {x}")
+    }
+    println!("x is {x}")
+}
+
+fn number_guesser() {
+    let _secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Guessxd");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed lol");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&_secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("TOo Big"),
+            Ordering::Equal => {
+                println!("DING DING DING CORRECT");
+                break;
+            }
+        }
+    }
 }
