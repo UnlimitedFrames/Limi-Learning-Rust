37c6ef057a545bc7c28d8ea54b7c8525cc4a3ef5
UnlimitedFrames <21beaps@gmail.com>
2022-08-02 17:56:21 +1200

    loads of stuff, csv files and the like

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-fn main() {
-let s = String::from("hey");
-println!("{s}");
+extern crate csv;
+use csv::{Error, StringRecord};
 
+
+fn main() -> Result<(), Box<Error>>{
+	let mut centeral_points = csv::Reader::from_path("src/cents.csv")?;
+	let mut populations =csv::Reader::from_path("src/pop.csv")?; 
+	for result in centeral_points.records() {
+		let record = result?;
+        println!("{:?}", record);
+	}
+	
+    Ok(())
 }
